f44c4a18fb1b4b916a34e640d4f5214eee9876c4
TylerGoeringer <tyler.goeringer@gmail.com>
2022-07-15 14:27:41 -0700

    Initial check-in

---
+++ CharacterOptionsPlus/CharacterOptionsPlus/Feats/SkaldsVigor.cs
@@ -0,0 +1,28 @@
+﻿
+using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+using BlueprintCore.Blueprints.References;
+using BlueprintCore.Utils;
+using Kingmaker.Blueprints.Classes;
+
+namespace CharacterOptionsPlus.Feats
+{
+  internal class SkaldsVigor
+  {
+    internal const string FeatureName = "SkaldsVigor";
+    internal const string FeatName = "SkaldsVigor";
+    internal const string FeatGuid = "55dd527b-8721-426b-aaa2-036ccc9a0458";
+
+    private static readonly LogWrapper Logger = LogWrapper.Get(FeatureName);
+
+    internal static void Configure()
+    {
+      Logger.Verbose($"Configuring {FeatureName}");
+
+      FeatureConfigurator.New(FeatureName, FeatGuid, FeatureGroup.Feat, FeatureGroup.CombatFeat)
+        .SetDisplayName("SkaldsVigor.Name")
+        .SetDescription("SkaldsVigor.Description")
+        .AddPrerequisiteFeature(FeatureRefs.RagingSong.ToString())
+        .Configure();
+    }
+  }
+}
---
+++ CharacterOptionsPlus/CharacterOptionsPlus/Main.cs
@@ -0,0 +1,63 @@
+﻿using BlueprintCore.Utils;
+using CharacterOptionsPlus.Feats;
+using HarmonyLib;
+using Kingmaker.Blueprints.JsonSystem;
+using System;
+using UnityModManagerNet;
+
+namespace CharacterOptionsPlus
+{
+  public static class Main
+  {
+    private static readonly LogWrapper Logger = LogWrapper.Get("CharacterOptionsPlus");
+
+    public static bool Load(UnityModManager.ModEntry modEntry)
+    {
+      try
+      {
+        var harmony = new Harmony(modEntry.Info.Id);
+        harmony.PatchAll();
+        Logger.Info("Finished patching.");
+      }
+      catch (Exception e)
+      {
+        Logger.Error("Failed to patch", e);
+      }
+      return true;
+    }
+
+    [HarmonyPatch(typeof(BlueprintsCache))]
+    static class BlueprintsCaches_Patch
+    {
+      private static bool Initialized = false;
+
+      [HarmonyPriority(Priority.Last)]
+      [HarmonyPatch(nameof(BlueprintsCache.Init)), HarmonyPostfix]
+      static void Postfix()
+      {
+        try
+        {
+          if (Initialized)
+          {
+            Logger.Info("Already initialized blueprints cache.");
+            return;
+          }
+          Initialized = true;
+
+          PatchFeats();
+        }
+        catch (Exception e)
+        {
+          Logger.Error("Failed to initialize.", e);
+        }
+      }
+
+      private static void PatchFeats()
+      {
+        Logger.Info("Patching feats.");
+
+        SkaldsVigor.Configure();
+      }
+    }
+  }
+}
