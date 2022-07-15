f01a922a7647b10facc0a4c03e33cefced515c01
TylerGoeringer <tyler.goeringer@gmail.com>
2022-07-15 16:11:43 -0700

    Initial Skalds Vigor implementation

--- CharacterOptionsPlus/CharacterOptionsPlus/Feats/SkaldsVigor.cs
+++ CharacterOptionsPlus/CharacterOptionsPlus/Feats/SkaldsVigor.cs
@@ -1,15 +1,26 @@
-﻿using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+﻿using BlueprintCore.Actions.Builder;
+using BlueprintCore.Actions.Builder.ContextEx;
+using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Buffs;
 using BlueprintCore.Blueprints.References;
 using BlueprintCore.Utils;
+using BlueprintCore.Utils.Types;
 using Kingmaker.Blueprints.Classes;
+using Kingmaker.EntitySystem.Stats;
 
 namespace CharacterOptionsPlus.Feats
 {
   internal class SkaldsVigor
   {
     internal const string FeatureName = "SkaldsVigor";
-    internal const string FeatName = "SkaldsVigor";
+    internal const string FeatureDisplayName = "SkaldsVigor.Name";
+    internal const string FeatureDescription = "SkaldsVigor.Description";
+
+    internal const string FeatName = "SkaldsVigor.Feat";
     internal const string FeatGuid = "55dd527b-8721-426b-aaa2-036ccc9a0458";
+
+    internal const string BuffName = "SkaldsVigor.Buff";
+    internal const string BuffGuid = "9e67121d-0433-4706-a107-7796187df3e1";
 
     private static readonly LogWrapper Logger = LogWrapper.Get(FeatureName);
 
@@ -17,10 +28,25 @@ namespace CharacterOptionsPlus.Feats
     {
       Logger.Verbose($"Configuring {FeatureName}");
 
-      FeatureConfigurator.New(FeatureName, FeatGuid, FeatureGroup.Feat, FeatureGroup.CombatFeat)
-        .SetDisplayName("SkaldsVigor.Name")
-        .SetDescription("SkaldsVigor.Description")
+      // Buff to apply fast healing
+      BuffConfigurator.New(BuffName, BuffGuid)
+        .SetDisplayName(FeatureDisplayName)
+        .SetDescription(FeatureDescription)
+        .AddEffectFastHealing(heal: 0, bonus: ContextValues.Rank())
+        .AddContextRankConfig(ContextRankConfigs.StatBonus(StatType.Strength))
+        .Configure();
+
+      // Skald's Vigor feat
+      FeatureConfigurator.New(FeatName, FeatGuid, FeatureGroup.Feat, FeatureGroup.CombatFeat)
+        .SetDisplayName(FeatureDisplayName)
+        .SetDescription(FeatureDescription)
         .AddPrerequisiteFeature(FeatureRefs.RagingSong.ToString())
+        .AddFactsChangeTrigger(
+          checkedFacts: new() { BuffRefs.InspiredRageBuff.ToString() },
+          onFactGainedActions:
+            ActionsBuilder.New().ApplyBuffPermanent(BuffName),
+          onFactLostActions:
+            ActionsBuilder.New().RemoveBuff(BuffName))
         .Configure();
     }
   }
