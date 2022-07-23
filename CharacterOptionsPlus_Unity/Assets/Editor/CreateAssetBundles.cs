2e58caf48f3021e0d7926238d8ff4f454b187393
TylerGoeringer <tyler.goeringer@gmail.com>
2022-07-23 11:21:13 -0700

    Add asset bundle for Skald's Vigor icons

---
+++ CharacterOptionsPlus_Unity/Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateAssetBundles
+{
+  [MenuItem("Assets/Build AssetBundles")]
+  static void BuildAllAssetBundles()
+  {
+    string assetBundleDirectory = "Assets/StreamingAssets";
+    if (!Directory.Exists(Application.streamingAssetsPath))
+    {
+      Directory.CreateDirectory(assetBundleDirectory);
+    }
+    BuildPipeline.BuildAssetBundles(
+        assetBundleDirectory,
+        BuildAssetBundleOptions.None,
+        EditorUserBuildSettings.activeBuildTarget);
+  }
+}
