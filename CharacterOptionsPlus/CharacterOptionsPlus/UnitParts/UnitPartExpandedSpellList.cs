3e76955c03af78121f757c85937f427a5ffee0ca
TylerGoeringer <tyler.goeringer@gmail.com>
2022-09-10 13:40:21 -0700

    Update UnitPartExpandedSpellList.cs
    - Handle the UnitPart being removed when the selection changes
    - Correctly populate SpellSelectionData.LevelCount
    - Load from the cache by blueprint lookup in case the local cache is cleared (e.g. when selection changes)
    - Add comments

--- CharacterOptionsPlus/CharacterOptionsPlus/UnitParts/UnitPartExpandedSpellList.cs
+++ CharacterOptionsPlus/CharacterOptionsPlus/UnitParts/UnitPartExpandedSpellList.cs
@@ -1,4 +1,5 @@
 ﻿using BlueprintCore.Blueprints.Configurators.Classes.Spells;
+using BlueprintCore.Utils;
 using CharacterOptionsPlus.Util;
 using HarmonyLib;
 using Kingmaker.Blueprints;
@@ -28,6 +29,9 @@ namespace CharacterOptionsPlus.UnitParts
     [JsonProperty]
     public Dictionary<BlueprintCharacterClassReference, List<SpellLevelList>> ExtraSpells = new();
 
+    /// <summary>
+    /// Add spells to the character's spell list.
+    /// </summary>
     public void AddSpells(
       BlueprintCharacterClassReference clazz, int level, params BlueprintAbilityReference[] spells)
     {
@@ -46,10 +50,12 @@ namespace CharacterOptionsPlus.UnitParts
       spellLevelList.m_Spells.AddRange(spells);
 
       // Update the cached spell list if it exists
-      if (ExpandedSpellLists.ContainsKey(clazz))
-        UpdateExpandedSpellList(clazz, spellLevelList);
+      UpdateExpandedSpellList(clazz, spellLevelList);
     }
 
+    /// <summary>
+    /// Remove spells from the character's spell list.
+    /// </summary>
     public void RemoveSpells(
       BlueprintCharacterClassReference clazz, int level, params BlueprintAbilityReference[] spells)
     {
@@ -63,10 +69,12 @@ namespace CharacterOptionsPlus.UnitParts
       spellLevelList.m_Spells = spellLevelList.m_Spells.Except(spells).ToList();
 
       // Update the cached spell list if it exists
-      if (ExpandedSpellLists.ContainsKey(clazz))
-        UpdateExpandedSpellList(clazz, spellLevelList);
+      UpdateExpandedSpellList(clazz, spellLevelList);
     }
 
+    /// <summary>
+    /// Returns a modified <c>SpellSelectionData</c> which uses the expanded spell list.
+    /// </summary>
     public SpellSelectionData GetSpellSelection(SpellSelectionData spellSelection)
     {
       if (!ExtraSpells.ContainsKey(spellSelection?.Spellbook.m_CharacterClass))
@@ -74,37 +82,82 @@ namespace CharacterOptionsPlus.UnitParts
       
       Logger.NativeLog(
         $"Returning spell selection with expanded spells for {Owner.CharacterName} - {spellSelection.Spellbook.m_CharacterClass}");
-      return new(
-        spellSelection.Spellbook,
-        GetExpandedSpellList(spellSelection.Spellbook.m_CharacterClass, spellSelection.SpellList));
+      var newSelection =
+        new SpellSelectionData(
+          spellSelection.Spellbook,
+          GetExpandedSpellList(spellSelection.Spellbook.m_CharacterClass, spellSelection.SpellList));
+      for (int i = 0; i < spellSelection.LevelCount.Length; i++)
+      {
+        newSelection.LevelCount[i] = spellSelection.LevelCount[i];
+      }
+      return newSelection;
     }
 
     private readonly Dictionary<BlueprintCharacterClassReference, BlueprintSpellList> ExpandedSpellLists =
       new();
+
+    /// <summary>
+    /// Returns the expanded spell list, either by fetching from the cache or creating it.
+    /// </summary>
     private BlueprintSpellList GetExpandedSpellList(
       BlueprintCharacterClassReference clazz, BlueprintSpellList spellList)
     {
-      if (ExpandedSpellLists.ContainsKey(clazz))
-        return ExpandedSpellLists[clazz];
+      var cachedSpellList = GetCachedSpellList(clazz);
+      if (cachedSpellList is not null)
+        return cachedSpellList;
 
       var guid = Guids.ReserveDynamic();
       Logger.NativeLog($"Creating expanded spell list for {Owner.CharacterName} - {clazz}, using dynamic guid {guid}");
 
       var extraSpells = ExtraSpells[clazz];
       var expandedSpellList =
-        SpellListConfigurator.New($"ExpandedSpellList_{Owner.CharacterName}_{clazz}", guid)
+        SpellListConfigurator.New(GetSpellListName(clazz), guid)
           .SetSpellsByLevel(Combine(spellList.SpellsByLevel, extraSpells))
           .Configure();
       ExpandedSpellLists.Add(clazz, expandedSpellList);
       return expandedSpellList;
     }
 
-    private void UpdateExpandedSpellList(BlueprintCharacterClassReference clazz, SpellLevelList newSpellLevelList)
+    /// <summary>
+    /// Fetches the cached spell list, or null if it doesn't exist.
+    /// </summary>
+    private BlueprintSpellList GetCachedSpellList(BlueprintCharacterClassReference clazz)
     {
-      Logger.NativeLog($"Updating expanded spell list for {Owner.CharacterName} - {clazz}");
-      Replace(ExpandedSpellLists[clazz].SpellsByLevel, newSpellLevelList);
+      if (ExpandedSpellLists.ContainsKey(clazz))
+        return ExpandedSpellLists[clazz];
+
+      if (BlueprintTool.TryGet<BlueprintSpellList>(GetSpellListName(clazz), out var existingSpellList))
+        return existingSpellList;
+
+      return null;
     }
 
+    /// <summary>
+    /// Updates the cached spell list if it exists.
+    /// </summary>
+    private void UpdateExpandedSpellList(BlueprintCharacterClassReference clazz, SpellLevelList newSpellLevelList)
+    {
+      var spellList = GetCachedSpellList(clazz);
+      if (spellList is null)
+        return;
+
+      Logger.NativeLog($"Updating expanded spell list for {Owner.CharacterName} - {clazz}");
+      Replace(spellList.SpellsByLevel, newSpellLevelList);
+    }
+
+    /// <summary>
+    /// Common name used to look up the spell list cache. Note that this means you cannot play on two characters in the
+    /// same game session with the same name and class that uses this unit part.
+    /// </summary>
+    private string GetSpellListName(BlueprintCharacterClassReference clazz)
+    {
+      return $"ExpandedSpellList_{Owner.CharacterName}_{clazz}";
+    }
+
+    /// <summary>
+    /// Replace (in-place) the spell level from <paramref name="baseList"/> of the corresponding level with
+    /// <paramref name="newList"/>.
+    /// </summary>
     private static void Replace(SpellLevelList[] baseList, SpellLevelList newList)
     {
       for (int i = 0; i < baseList.Length; i++)
@@ -119,6 +172,9 @@ namespace CharacterOptionsPlus.UnitParts
         $"Cannot add extra spell with level {newList.SpellLevel} which is not in the base list.");
     }
 
+    /// <summary>
+    /// Returns a combined spell level list with the <paramref name="extraSpells"/> added.
+    /// </summary>
     private static SpellLevelList[] Combine(SpellLevelList[] baseList, List<SpellLevelList> extraSpells)
     {
       var spellLevelList = new SpellLevelList[baseList.Length];
@@ -138,16 +194,19 @@ namespace CharacterOptionsPlus.UnitParts
       return spellLevelList;
     }
 
+    /// <summary>
+    /// Patch responsible for swapping the selection data with the expanded version before it is bound / viewed in the
+    /// level up UI.
+    /// </summary>
     [HarmonyPatch(typeof(CharGenSpellsPhaseVM))]
     static class CharGenSpellsPhaseVM_Patch
     {
-      // TODO: Figure out why this doesn't work. Probably need to muck w/ Spellbook?
       [HarmonyPatch(nameof(CharGenSpellsPhaseVM.OnBeginDetailedView)), HarmonyPrefix]
       static void OnBeginDetailedView(CharGenSpellsPhaseVM __instance)
       {
         try
         {
-          Logger.NativeLog("Swapping spell selection data");
+          Logger.NativeLog($"Swapping spell selection data.");
           __instance.m_SelectionData =
             __instance.m_UnitDescriptor.Value.Ensure<UnitPartExpandedSpellList>()
               .GetSpellSelection(__instance.m_SelectionData);
@@ -187,7 +246,15 @@ namespace CharacterOptionsPlus.UnitParts
       SourceSpellList = sourceSpellList;
     }
 
-    // TODO: This is called on every selection and OnDeactivate is NOT. Need to fix.
+    /// <summary>
+    /// Adds the selected spell to <see cref="UnitPartExpandedSpellList"/>.
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// This is actually called in the level up UI every time a spell is selected. This actually doesn't matter because
+    /// when the user changes the selection, the UnitPart is reset so there won't be any issues with multiple spells
+    /// being saved as available.
+    /// </remarks>
     public override void OnActivate()
     {
       try
