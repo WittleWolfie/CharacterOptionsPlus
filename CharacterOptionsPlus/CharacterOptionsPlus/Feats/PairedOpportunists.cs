4701013cf3083182f5a5c503c45c383af2d61c15
TylerGoeringer <tyler.goeringer@gmail.com>
2022-09-18 13:15:49 -0700

    Create component
    Nothing in game really allows "constructing" this logic so just create a new component.

--- CharacterOptionsPlus/CharacterOptionsPlus/Feats/PairedOpportunists.cs
+++ CharacterOptionsPlus/CharacterOptionsPlus/Feats/PairedOpportunists.cs
@@ -23,6 +23,14 @@ using System.Threading.Tasks;
 using TabletopTweaks.Core.NewComponents.Prerequisites;
 using static UnityModManagerNet.UnityModManager.ModEntry;
 using Kingmaker.Blueprints.Classes;
+using Kingmaker.RuleSystem.Rules;
+using Kingmaker.Blueprints.Facts;
+using BlueprintCore.Utils;
+using Kingmaker.Controllers.Combat;
+using Kingmaker.EntitySystem.Entities;
+using Kingmaker;
+using Kingmaker.Blueprints.Classes.Selection;
+using BlueprintCore.Blueprints.References;
 
 namespace CharacterOptionsPlus.Feats
 {
@@ -64,7 +72,94 @@ namespace CharacterOptionsPlus.Feats
         .SetDisplayName(FeatDisplayName)
         .SetDescription(FeatDescription)
         //.SetIcon(IconName)
+        .AddFeatureTagsComponent(FeatureTag.Melee | FeatureTag.Attack | FeatureTag.Teamwork)
+        .AddRecommendationWeaponSubcategoryFocus(WeaponSubCategory.Melee)
+        .AddRecommendationThreeQuartersBAB()
+        .AddRecommendationHasFeature(FeatureRefs.SoloTactics.ToString())
+        .AddRecommendationHasFeature(FeatureRefs.InquisitorSoloTactician.ToString())
+        .AddComponent<PairedOpportunistsComponent>()
         .Configure(delayed: true);
+    }
+
+    [TypeId("ce4218b8-27b6-4484-93df-2458aa7ae788")]
+    public class PairedOpportunistsComponent
+      : UnitFactComponentDelegate, IInitiatorRulebookHandler<RuleCalculateAttackBonus>, IAttackOfOpportunityHandler
+    {
+      private static readonly Feet Adjacency = new(5);
+
+      private BlueprintUnitFact _pairedOpportunists;
+      private BlueprintUnitFact PairedOpportunists
+      {
+        get
+        {
+          _pairedOpportunists ??= BlueprintTool.Get<BlueprintUnitFact>(Guids.PairedOpportunistsFeat);
+          return _pairedOpportunists;
+        }
+      }
+
+      public void OnEventAboutToTrigger(RuleCalculateAttackBonus evt)
+      {
+        if (evt.Reason.Rule is not RuleAttackWithWeapon attack || !attack.IsAttackOfOpportunity)
+        {
+          Logger.NativeLog("Skipping: Not AOO.");
+          return;
+        }
+
+        if (Owner.State.Features.SoloTactics)
+        {
+          AddAttackBonus(evt);
+          return;
+        }
+
+        foreach (var unit in GameHelper.GetTargetsAround(Owner.Position, Adjacency))
+        {
+          if (unit.IsAlly(Owner) && unit.Descriptor.HasFact(PairedOpportunists) && unit.IsEngage(evt.Target))
+          {
+            AddAttackBonus(evt);
+            return;
+          }
+        }
+
+        Logger.NativeLog("Skipping: No supporting ally.");
+      }
+
+      private static void AddAttackBonus(RuleCalculateAttackBonus evt)
+      {
+        Logger.NativeLog("Adding Paired Opportunists attack bonus.");
+        evt.AddModifier(new(4, ModifierDescriptor.Circumstance));
+      }
+
+      public void HandleAttackOfOpportunity(UnitEntityData attacker, UnitEntityData target)
+      {
+        if (attacker == Owner)
+        {
+#if DEBUG
+          Logger.NativeLog("Not Provoking: Attacker is owner.");
+#endif
+          return;
+        }
+
+        if (!attacker.IsAlly(Owner) || attacker.Descriptor.HasFact(PairedOpportunists))
+        {
+#if DEBUG
+          Logger.NativeLog("Not Provoking: No supporting ally.");
+#endif
+          return;
+        }
+
+        if (!Owner.IsEngage(target))
+        {
+#if DEBUG
+          Logger.NativeLog("Not Provoking: Not engaged with target.");
+#endif
+          return;
+        }
+
+        Logger.NativeLog($"{attacker.CharacterName} provoked an attack against {target.CharacterName}");
+        Game.Instance?.CombatEngagementController.ForceAttackOfOpportunity(Owner, target);
+      }
+
+      public void OnEventDidTrigger(RuleCalculateAttackBonus evt) { }
     }
   }
 }
