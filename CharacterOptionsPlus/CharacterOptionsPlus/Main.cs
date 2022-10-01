85a7effd5a006717d5e226b87d8aab86458abca1
TylerGoeringer <tyler.goeringer@gmail.com>
2022-10-01 10:35:34 -0700

    Update Main.cs

--- CharacterOptionsPlus/CharacterOptionsPlus/Main.cs
+++ CharacterOptionsPlus/CharacterOptionsPlus/Main.cs
@@ -105,6 +105,7 @@ namespace CharacterOptionsPlus
       {
         Logger.Log("Configuring feats.");
 
+        EldritchHeritage.Configure();
         FuriousFocus.Configure();
         GloriousHeat.Configure();
         Hurtful.Configure();
