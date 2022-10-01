f45436a597e1c2b0233b1afb8525b76f743ee931
TylerGoeringer <tyler.goeringer@gmail.com>
2022-10-01 10:35:51 -0700

    Add resource logic & fix buff

--- CharacterOptionsPlus/CharacterOptionsPlus/Feats/EldritchHeritage.cs
+++ CharacterOptionsPlus/CharacterOptionsPlus/Feats/EldritchHeritage.cs
@@ -1,36 +1,20 @@
-﻿using BlueprintCore.Actions.Builder;
-using BlueprintCore.Blueprints.Configurators.Classes.Selection;
+﻿using BlueprintCore.Blueprints.Configurators.Classes.Selection;
 using BlueprintCore.Blueprints.Configurators.UnitLogic.ActivatableAbilities;
 using BlueprintCore.Blueprints.CustomConfigurators.Classes;
 using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Buffs;
 using BlueprintCore.Blueprints.References;
-using BlueprintCore.Conditions.Builder;
-using BlueprintCore.Conditions.Builder.BasicEx;
-using BlueprintCore.Utils.Types;
 using CharacterOptionsPlus.Util;
 using Kingmaker.Blueprints.Classes;
-using Kingmaker.Blueprints.Classes.Prerequisites;
 using Kingmaker.Blueprints.Classes.Selection;
-using Kingmaker.Blueprints.Classes.Spells;
 using Kingmaker.Blueprints.JsonSystem;
-using Kingmaker.Designers;
 using Kingmaker.EntitySystem.Stats;
-using Kingmaker.Enums;
-using Kingmaker.PubSubSystem;
-using Kingmaker.RuleSystem;
-using Kingmaker.RuleSystem.Rules;
-using Kingmaker.RuleSystem.Rules.Abilities;
-using Kingmaker.RuleSystem.Rules.Damage;
 using Kingmaker.UnitLogic;
 using Kingmaker.UnitLogic.ActivatableAbilities;
 using Kingmaker.UnitLogic.Buffs;
 using Kingmaker.UnitLogic.Buffs.Blueprints;
 using Kingmaker.UnitLogic.Buffs.Components;
-using Kingmaker.UnitLogic.FactLogic;
-using Kingmaker.Utility;
 using System;
-using System.Linq;
-using TabletopTweaks.Core.NewComponents.Prerequisites;
+using static Kingmaker.UnitLogic.ActivatableAbilities.ActivatableAbilityResourceLogic;
 using static UnityModManagerNet.UnityModManager.ModEntry;
 
 namespace CharacterOptionsPlus.Feats
@@ -45,10 +29,9 @@ namespace CharacterOptionsPlus.Feats
     private const string AbyssalHeritageName = "EldrichHeritage.Abyssal";
     private const string AbyssalHeritageClaws = "EldritchHeritage.Abyssal.Claws";
     private const string AbyssalHeritageClawsBuff = "EldritchHeritage.Abyssal.Claws.Buff";
-    private const string AbyssalHeritageClawsResource = "EldrtichHeritrage.Abyssal.Claws.Resource";
 
     private const string IconPrefix = "assets/icons/";
-    private const string Icon = IconPrefix + "eldritchheritage.png";
+    private const string Icon = IconPrefix + "gloriousheat.png";
 
     private static readonly ModLogger Logger = Logging.GetLogger(FeatName);
 
@@ -106,8 +89,6 @@ namespace CharacterOptionsPlus.Feats
         .AddComponent<AbyssalHeritageClawsComponent>()
         .Configure();
 
-      // TODO: Resources!
-
       var abyssalClaws = ActivatableAbilityRefs.BloodlineAbyssalClawsAbililyLevel1.Reference.Get();
       var ability = ActivatableAbilityConfigurator.New(AbyssalHeritageClaws, Guids.AbyssalHeritageClawsAbility)
         .SetDisplayName(abyssalClaws.m_DisplayName)
@@ -117,7 +98,9 @@ namespace CharacterOptionsPlus.Feats
         .SetDeactivateIfOwnerDisabled()
         .SetDeactivateImmediately()
         .SetActivationType(AbilityActivationType.WithUnitCommand)
-        .AddActivatableAbilityResourceLogic(null)
+        .AddActivatableAbilityResourceLogic(
+          spendType: ResourceSpendType.NewRound,
+          requiredResource: AbilityResourceRefs.BloodlineAbyssalClawsResource.ToString())
         .SetBuff(buff)
         .Configure();
 
@@ -130,6 +113,8 @@ namespace CharacterOptionsPlus.Feats
         .AddPrerequisiteFeature(FeatureRefs.SkillFocusPhysique.ToString())
         .AddPrerequisiteNoFeature(FeatureRefs.AbyssalBloodlineRequisiteFeature.ToString())
         .AddFacts(new() { ability })
+        .AddAbilityResources(
+          resource: AbilityResourceRefs.BloodlineAbyssalClawsResource.ToString(), restoreAmount: true)
         .Configure();
     }
 
@@ -144,6 +129,8 @@ namespace CharacterOptionsPlus.Feats
           buff = Owner.AddBuff(BuffRefs.BloodlineAbyssalClawsBuffLevel1.Reference.Get(), Context);
         else if (characterLevel < 9)
           buff = Owner.AddBuff(BuffRefs.BloodlineAbyssalClawsBuffLevel2.Reference.Get(), Context);
+        else if (characterLevel < 13)
+          buff = Owner.AddBuff(BuffRefs.BloodlineAbyssalClawsBuffLevel3.Reference.Get(), Context);
         else
           buff = Owner.AddBuff(BuffRefs.BloodlineAbyssalClawsBuffLevel4.Reference.Get(), Context);
 
