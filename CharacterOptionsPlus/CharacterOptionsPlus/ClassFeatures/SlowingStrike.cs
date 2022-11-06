3110a21d46fe6bb3a6cb06566bf0cc1c40f2e199
TylerGoeringer <tyler.goeringer@gmail.com>
2022-11-06 10:01:33 -0800

    Implement mechanics

--- CharacterOptionsPlus/CharacterOptionsPlus/ClassFeatures/SlowingStrike.cs
+++ CharacterOptionsPlus/CharacterOptionsPlus/ClassFeatures/SlowingStrike.cs
@@ -1,7 +1,16 @@
-﻿using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+﻿using BlueprintCore.Actions.Builder;
+using BlueprintCore.Actions.Builder.ContextEx;
+using BlueprintCore.Blueprints.CustomConfigurators.Classes;
 using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Buffs;
+using BlueprintCore.Blueprints.References;
+using BlueprintCore.Conditions.Builder;
+using BlueprintCore.Conditions.Builder.ContextEx;
+using BlueprintCore.Utils.Types;
 using CharacterOptionsPlus.Util;
 using Kingmaker.Blueprints.Classes;
+using Kingmaker.EntitySystem.Stats;
+using Kingmaker.RuleSystem;
+using Kingmaker.UnitLogic;
 using System;
 using static UnityModManagerNet.UnityModManager.ModEntry;
 
@@ -17,7 +26,7 @@ namespace CharacterOptionsPlus.ClassFeatures
     private const string BuffName = "SlowingStrike.Buff";
 
     private const string IconPrefix = "assets/icons/";
-    private const string IconName = IconPrefix + "burningdisarm.png";
+    private const string IconName = IconPrefix + "burningdisarm.png"; // TODO:
 
     private static readonly ModLogger Logger = Logging.GetLogger(FeatureName);
 
@@ -52,6 +61,7 @@ namespace CharacterOptionsPlus.ClassFeatures
         .SetDisplayName(DisplayName)
         .SetDescription(Description)
         .SetIcon(IconName)
+        .AddCondition(condition: UnitCondition.Slowed)
         .Configure();
 
       FeatureConfigurator.New(FeatureName, Guids.SlowingStrikeTalent, FeatureGroup.SlayerTalent)
@@ -59,6 +69,24 @@ namespace CharacterOptionsPlus.ClassFeatures
         .SetDescription(Description)
         .SetIcon(IconName)
         .SetIsClassFeature()
+        .SetReapplyOnLevelUp()
+        .AddRecalculateOnStatChange(stat: StatType.Intelligence)
+        .AddPrerequisiteClassLevel(CharacterClassRefs.SlayerClass.ToString(), level: 4)
+        .AddContextCalculateAbilityParamsBasedOnClass(
+          CharacterClassRefs.SlayerClass.ToString(), statType: StatType.Intelligence)
+        .AddInitiatorAttackRollTrigger(
+          onlyHit: true,
+          sneakAttack: true,
+          action: ActionsBuilder.New()
+            .SavingThrow(
+              SavingThrowType.Fortitude,
+              onResult: ActionsBuilder.New()
+                .ConditionalSaved(
+                  failed: ActionsBuilder.New()
+                    .Conditional(
+                      conditions: ConditionsBuilder.New().HasFact(buff),
+                      ifTrue: ActionsBuilder.New().IncreaseBuffDuration(ContextDuration.FixedDice(DiceType.D4), buff),
+                      ifFalse: ActionsBuilder.New().ApplyBuff(buff, ContextDuration.FixedDice(DiceType.D4))))))
         .Configure(delayed: true);
     }
   }
