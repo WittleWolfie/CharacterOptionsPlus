9019a924ac19a7951e55e3ed438c9ebee25475a5
TylerGoeringer <tyler.goeringer@gmail.com>
2022-12-09 13:10:21 -0800

    Prevent re-adding abilities on save / load

--- CharacterOptionsPlus/CharacterOptionsPlus/Components/AlwaysAddToActionBar.cs
+++ CharacterOptionsPlus/CharacterOptionsPlus/Components/AlwaysAddToActionBar.cs
@@ -1,10 +1,12 @@
 ﻿using CharacterOptionsPlus.Util;
 using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.UI.UnitSettings;
 using Kingmaker.UnitLogic;
 using Kingmaker.UnitLogic.Abilities.Blueprints;
 using Kingmaker.UnitLogic.ActivatableAbilities;
 using System;
+using System.Linq;
 using static UnityModManagerNet.UnityModManager.ModEntry;
 
 namespace CharacterOptionsPlus.Components
@@ -20,7 +22,18 @@ namespace CharacterOptionsPlus.Components
     {
       try
       {
-        Owner.UISettings.RemoveFromAlreadyAutomaticallyAdded((BlueprintAbility)Fact.Blueprint);
+        if (!Owner.UISettings.m_Slots.Any(
+          slot =>
+          {
+            if (slot is MechanicActionBarSlotAbility ability)
+              return ability.Ability.Fact == Fact;
+            if (slot is MechanicActionBarSlotActivableAbility activatable)
+              return activatable.ActivatableAbility.SourceFact == Fact;
+            return false;
+          }))
+        {
+          Owner.UISettings.RemoveFromAlreadyAutomaticallyAdded((BlueprintAbility)Fact.Blueprint);
+        }
         base.OnTurnOn();
       }
       catch (Exception e)
