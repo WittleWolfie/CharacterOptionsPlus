3b101e824e5a126a1c5117971a4d57a18debe032
TylerGoeringer <tyler.goeringer@gmail.com>
2022-12-16 14:54:00 -0800

    Fix implementation to work when centered on the caster

--- CharacterOptionsPlus/CharacterOptionsPlus/Components/AbilityDeliverBurst.cs
+++ CharacterOptionsPlus/CharacterOptionsPlus/Components/AbilityDeliverBurst.cs
@@ -1,27 +1,47 @@
-﻿using Kingmaker.Blueprints.JsonSystem;
+﻿using CharacterOptionsPlus.Util;
+using Kingmaker.Blueprints.JsonSystem;
 using Kingmaker.Designers;
 using Kingmaker.UnitLogic.Abilities;
 using Kingmaker.UnitLogic.Abilities.Components.Base;
 using Kingmaker.Utility;
 using System.Collections.Generic;
-using System.Linq;
+using static UnityModManagerNet.UnityModManager.ModEntry;
 
 namespace CharacterOptionsPlus.Components
 {
   [TypeId("bed832dd-3701-4d7b-9f66-552bf89a8cfe")]
   internal class AbilityDeliverBurst : AbilityDeliverEffect
   {
-    private readonly Feet Radius;
+    private static readonly ModLogger Logger = Logging.GetLogger(nameof(AbilityDeliverBurst));
 
-    internal AbilityDeliverBurst(Feet radius)
+    private readonly Feet Radius;
+    private readonly bool IncludeCaster;
+    private readonly bool CenterOnCaster;
+
+    internal AbilityDeliverBurst(Feet radius, bool includeCaster = false, bool centerOnCaster = true)
     {
       Radius = radius;
+      IncludeCaster = includeCaster;
+      CenterOnCaster = centerOnCaster;
     }
 
     public override IEnumerator<AbilityDeliveryTarget> Deliver(AbilityExecutionContext context, TargetWrapper target)
     {
-      return (IEnumerator<AbilityDeliveryTarget>)GameHelper.GetTargetsAround(target.m_Point, Radius)
-        .Select(unit => new AbilityDeliveryTarget(unit));
+      var caster = context.MaybeCaster;
+      if (caster is null)
+      {
+        Logger.Warning("No caster");
+        yield break;
+      }
+
+      var targetPoint = CenterOnCaster ? caster.Position : target.m_Point;
+      foreach (var unit in GameHelper.GetTargetsAround(targetPoint, Radius))
+      {
+        if (unit == context.MaybeCaster && !IncludeCaster)
+          continue;
+        yield return new(unit);
+      }
+      yield break;
     }
   }
 }
